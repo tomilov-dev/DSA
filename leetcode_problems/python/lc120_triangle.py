dd17ab7e3658546097b7e77b007e5453cfee9b42
IvanTom2 <tomilov.vana@gmail.com>
2025-04-28 15:27:24 +0500

    added new dp solution

--- leetcode_problems/python/lc120_triangle.py
+++ leetcode_problems/python/lc120_triangle.py
@@ -1,28 +1,17 @@
-from pprint import pprint
-
-
 class Solution:
     def minimumTotal(
         self,
         triangle: list[list[int]],
     ) -> int:
-        n = len(triangle)
-        dp = triangle[-1][:]
-
-        pprint(dp)
-        for i in range(n - 2, -1, -1):
-            for j in range(i + 1):
-                dp[j] = min(dp[j], dp[j + 1]) + triangle[i][j]
-                pprint(dp)
-
-        return dp[0]
+        dp = triangle[-1]
+        for i in range(len(triangle) - 2, -1, -1):
+            new_dp = triangle[i]
+            for j in range(len(new_dp)):
+                new_dp[j] += min(dp[j], dp[j + 1])
+            dp = new_dp
+        return max(dp)
 
 
 if __name__ == "__main__":
-    triangle = [
-        [2],
-        [3, 4],
-        [6, 5, 7],
-        [4, 1, 8, 3],
-    ]
+    triangle = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
     print(Solution().minimumTotal(triangle))
