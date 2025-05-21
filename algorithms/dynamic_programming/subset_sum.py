b5f9ec37b710ce026e7f4d5ab0a1695ea35e279a
tomilov-dev <tomilov.dev@gmail.com>
2025-05-21 17:23:53 +0500

    solved problem

--- algorithms/dynamic_programming/subset_sum.py
+++ algorithms/dynamic_programming/subset_sum.py
@@ -1,17 +1,3 @@
-class Solution:
-    def isSubsetSum(
-        self,
-        arr: list[int],
-        sum: int,
-    ) -> bool:
-        dp = [False] * (sum + 1)
-        dp[0] = True
-        for num in arr:
-            for i in range(sum, num - 1, -1):
-                dp[i] = dp[i] or dp[i - num]
-        return dp[sum]
-
-
 class SolutionRecursive:
     def isSubsetSum(
         self,
@@ -112,8 +98,8 @@ class SolutionBottomUpSuperOptimized:
 if __name__ == "__main__":
     arr = [3, 34, 4, 12, 5, 2]
     sum = 9
-    print(Solution().isSubsetSum(arr, sum))
     print(SolutionRecursive().isSubsetSum(arr, sum))
     print(SolutionTopDown().isSubsetSum(arr, sum))
     print(SolutionBottomUp().isSubsetSum(arr, sum))
+    print(SolutionBottomUpOptimized().isSubsetSum(arr, sum))
     print(SolutionBottomUpSuperOptimized().isSubsetSum(arr, sum))
