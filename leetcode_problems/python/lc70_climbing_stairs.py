0e0eebbc23b0034159dc9e611f3017d15e5a1c47
tomilov-dev <tomilov.dev@gmail.com>
2025-09-16 14:13:02 +0500

    feat: lc70 updated

--- leetcode_problems/python/lc70_climbing_stairs.py
+++ leetcode_problems/python/lc70_climbing_stairs.py
@@ -13,7 +13,7 @@ Explanation: There are two ways to climb to the top.
 """
 
 
-class Solution1(object):
+class Solution1:
     """
     Too slow: Runtime Error. Recursive computing with redundant calculations.
     """
@@ -24,24 +24,22 @@ class Solution1(object):
         return self.run(n - 1) + self.run(n - 2)
 
 
-class Solution2(object):
+class Solution2:
     """
     37 ms, 16.2 MB
     """
 
     def recursion(self, n: int, memo: dict) -> int:
-        if n == 0 or n == 1:
-            return 1
-        elif n not in memo:
+        if n not in memo:
             memo[n] = self.recursion(n - 1, memo) + self.recursion(n - 2, memo)
         return memo[n]
 
     def run(self, n: int) -> int:
-        memo = {}
+        memo = {0: 1, 1: 1}
         return self.recursion(n, memo)
 
 
-class Solution3(object):
+class Solution3:
     """
     38 ms, 16.2 MB
     """
@@ -50,16 +48,14 @@ class Solution3(object):
         if n == 0 or n == 1:
             return 1
 
-        table = [0] * (n + 1)
-        table[0] = table[1] = 1
-
+        dp = [1] * (n + 1)
         for index in range(2, n + 1):
-            table[index] = table[index - 1] + table[index - 2]
+            dp[index] = dp[index - 1] + dp[index - 2]
 
-        return table[n]
+        return dp[n]
 
 
-class Solution4(object):
+class Solution4:
     """
     37 ms, 16.2 MB
     """
@@ -68,55 +64,11 @@ class Solution4(object):
         if n == 0 or n == 1:
             return 1
 
-        curr, prev = 1, 1
+        curr = 1
+        prev = 1
         for _ in range(2, n + 1):
             temp = curr
             curr = prev + curr
             prev = temp
 
         return curr
-
-
-class WrongInPythonSolution5(object):
-    def run(self, n: int) -> int:
-        if n <= 0:
-            return 0
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-
-        all_ways, curr, prev = 0, 1, 2
-        for _ in range(2, n + 1):
-            all_ways = curr + prev
-            prev = curr
-            curr = all_ways
-
-        return n
-
-
-class SolutionBottomUp:
-    def climbStairs(self, n: int) -> int:
-        dp = [0] * (n + 1)
-        dp[0] = 1
-        dp[1] = 1
-        for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[n]
-
-
-if __name__ == "__main__":
-    n = 4
-    print(SolutionBottomUp().climbStairs(n))
-
-    # sol1 = Solution1()
-    # sol2 = Solution2()
-    # sol3 = Solution3()
-    # sol4 = Solution4()
-    # sol5 = WrongInPythonSolution5()
-
-    # sol1.run(n)
-    # sol2.run(n)
-    # sol3.run(n)
-    # sol4.run(n)
-    # sol5.run(n)
