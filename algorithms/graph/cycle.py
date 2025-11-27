fe5f21d4b469b6e925fbc68b50a258db39649aa0
tomilov-dev <tomilov.dev@gmail.com>
2025-11-27 13:25:56 +0500

    feat: added solutions on direct/undirect shortest paths

--- algorithms/graph/cycle.py
+++ algorithms/graph/cycle.py
@@ -1,3 +1,12 @@
+import sys
+from pathlib import Path
+
+
+ROOT_DIR = Path(__file__).parent.parent.parent
+sys.path.append(str(ROOT_DIR))
+from data_structures.union_find import UnionFind
+
+
 def undirect(graph: list[list[int]]) -> bool:
     def dfs(v: int, parent: int) -> bool:
         color[v] = 1
@@ -19,6 +28,18 @@ def undirect(graph: list[list[int]]) -> bool:
     for v in range(n):
         if color[v] == 0 and dfs(v, -1):
             return True
+    return False
+
+
+def undirect_uf(graph: list[list[int]]) -> bool:
+    n = len(graph)
+    uf = UnionFind(n)
+    for u in range(n):
+        for v, conn in enumerate(graph[u]):
+            if conn == 1 and u < v:
+                if uf.find(u) == uf.find(v):
+                    return True
+                uf.union(u, v)
     return False
 
 
